bb11264c8aaaa5e45361b2c2014eda23ce388c9f
VeronicaManchola <veronica.manchola@hotmail.com>
2018-12-26 15:17:57 -0300

    imprime numero, nombre, imagen y tipos

--- src/data.js
+++ src/data.js
@@ -1,11 +1,51 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
-};
+// const example = () => {
+//   return 'example';
+// };
 
-window.example = example;
+// window.example = example;
 
+const data = POKEMON.pokemon;
 
-//Testeando merge de cambios de master a Andrea
+const printId = (data) => {
+  let id = [];
+  for(i = 0; i < data.length; i++){
+    id.push(data[i].id)
+  };
+  return id;
+  }
+
+const printNumber = (data) => {
+  let number = [];
+  for(i = 0; i < data.length; i++){
+    number.push(data[i].num)
+  };
+  return number;
+  }
+
+const printName = (data) => {
+  let name = [];
+  for(i = 0; i < data.length; i++){
+    name.push(data[i].name)
+  };
+  return name;
+  }
+
+const printImage = (data) => {
+  let image = [];
+  for(i = 0; i < data.length; i++){
+    image.push(data[i].img)
+  };
+  return image;
+  }
+
+const printType = (data) => {
+  let type = [];
+  for(i = 0; i < data.length; i++){
+    type.push(data[i].type)
+  };
+  return type;
+  }
+
