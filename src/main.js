f1737b1f84b9927ea7993f4761bf66c088acb5b6
VeronicaManchola <veronica.manchola@hotmail.com>
2019-01-04 09:14:43 -0300

    Agregando comentario

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 let mainBox = document.getElementById("root");
 let createCard;
 
+//Imprime tarjetas al cargar la pagina
 window.onload=function(){
 
   document.getElementById("root").innerHTML = "";
