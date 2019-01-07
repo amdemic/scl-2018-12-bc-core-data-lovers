71f1c1cb5398638fa6023cb747eb1809bc6539d5
VeronicaManchola <veronica.manchola@hotmail.com>
2019-01-07 18:18:02 -0300

    acorta funciones, mejora diseño, agrega favicon

--- src/data.js
+++ src/data.js
@@ -1,96 +1,64 @@
 const data = POKEMON.pokemon;
 
 
-
-function sortedAz(data){ 
-  const sortedData = data;
-  data.sort(function(a, b){
-    if(a.name < b.name){
-      return -1;
-    }
-    if(a.name > b.name){
-      return 1;
-    }
-  })
-  return sortedData;
+function sortData(data, selectedOrder) {
+  if (selectedOrder === "0" || selectedOrder === "lower") {
+    const sortedData = data;
+    data.sort(function (a, b) {
+      if (a.num < b.num) {
+        return -1;
+      }
+      if (a.num > b.num) {
+        return 1;
+      }
+    })
+    return sortedData;
+  } else if (selectedOrder === "higher") {
+    const sortedData = data;
+    data.sort(function (a, b) {
+      if (b.num < a.num) {
+        return -1;
+      }
+      if (b.num > a.num) {
+        return 1;
+      }
+    })
+    return sortedData;
+  } else if (selectedOrder === "az") {
+    const sortedData = data;
+    data.sort(function (a, b) {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+    })
+    return sortedData;
+  } else if (selectedOrder === "za") {
+    const sortedData = data;
+    data.sort(function (a, b) {
+      if (b.name < a.name) {
+        return -1;
+      }
+      if (b.name > a.name) {
+        return 1;
+      }
+    })
+    return sortedData;
+  }
 };
 
-function sortedZa(data){
-  const sortedData = data;
-  data.sort(function(a, b){
-    if(b.name < a.name){
-      return -1;
-    }
-    if(b.name > a.name){
-      return 1;
-    }
+function filterData(data, selectedType) {
+  if (selectedType === "0") {
+    return data;
+  };
+  const filteredData = data.filter(element => {
+    return element.type[0] === selectedType || element.type[1] === selectedType;
   })
-  return sortedData;
+  return filteredData;
 };
 
-function sortedLower(data){
-  const sortedData = data;
-  data.sort(function(a, b){
-    if(a.num < b.num){
-      return -1;
-    }
-    if(a.num > b.num){
-      return 1;
-    }
-  })
-  return sortedData;
-};
-
-function sortedHigher(data){
-  const sortedData = data;
-  data.sort(function(a, b){
-    if(b.num < a.num){
-      return -1;
-    }
-    if(b.num > a.num){
-      return 1;
-    }
-  })
-  return sortedData;
-};
-
-
-const printId = (newData) => {
-  let id = [];
-  for(i = 0; i < newData.length; i++){
-    id.push(newData[i].id)
-  };
-  return id;
-}
-
-const printNumber = (newData) => {
-  let number = [];
-  for(i = 0; i < newData.length; i++){
-    number.push(newData[i].num)
-  };
-  return number;
-}
-
-const printName = (newData) => {
-  let name = [];
-  for(i = 0; i < newData.length; i++){
-    name.push(newData[i].name)
-  };
-  return name;
-}
-
-const printImage = (newData) => {
-  let image = [];
-  for(i = 0; i < newData.length; i++){
-    image.push(newData[i].img)
-  };
-  return image;
-}
-
-const printType = (newData) => {
-  let type = [];
-  for(i = 0; i < newData.length; i++){
-    type.push(newData[i].type)
-  };
-  return type;
+function computeStats(data){
+  return data.length;
 }
--- src/main.js
+++ src/main.js
@@ -1,191 +1,107 @@
 let mainBox = document.getElementById("root");
 let createCard;
+let createTotal;
 
 //Imprime tarjetas al cargar la pagina
-window.onload=function(){
+window.onload = function () {
 
   document.getElementById("root").innerHTML = "";
-  
-  const newData = sortedLower(data);
 
-  const idResult = printId(newData);
-  const numberResult = printNumber(newData);
-  const nameResult = printName(newData);
-  const imageResult = printImage(newData);
-  const typeResult = printType(newData);
+  let selectedType = document.getElementById("type").value;
+  const newData = filterData(data, selectedType);
+  const totalPokemon = computeStats(newData);
 
+  createTotal = document.createElement("h5");
+  mainBox.appendChild(createTotal);
 
-  for(i=0; i < idResult.length; i++){
-    let finalType = [];
-    createCard = document.createElement("div");
-    //createCard.className = "col l5";
-    mainBox.appendChild(createCard);
-    for(a=0; a < typeResult[i].length; a++){
-      if(typeResult[i][a] === "Grass"){
-        finalType.push("Planta");
-      }else if(typeResult[i][a] === "Poison"){
-        finalType.push("Veneno");
-      }else if(typeResult[i][a] === "Fire"){
-        finalType.push("Fuego");
-      }else if(typeResult[i][a] === "Bug"){
-        finalType.push("Bicho");
-      }else if(typeResult[i][a] === "Flying"){
-        finalType.push("Volador");
-      }else if(typeResult[i][a] === "Normal"){
-        finalType.push("Normal");
-      }else if(typeResult[i][a] === "Electric"){
-        finalType.push("Eléctrico");
-      }else if(typeResult[i][a] === "Ground"){
-        finalType.push("Tierra");
-      }else if(typeResult[i][a] === "Water"){
-        finalType.push("Agua");
-      }else if(typeResult[i][a] === "Fighting"){
-        finalType.push("Lucha");
-      }else if(typeResult[i][a] === "Psychic"){
-        finalType.push("Psíquico");
-      }else if(typeResult[i][a] === "Rock"){
-        finalType.push("Roca");
-      }else if(typeResult[i][a] === "Ghost"){
-        finalType.push("Fantasma");
-      }else if(typeResult[i][a] === "Ice"){
-        finalType.push("Hielo");
-      }else if(typeResult[i][a] === "Dragon"){
-        finalType.push("Dragón");
-      }
-    }
-    createCard.innerHTML += 
-    `<div class="col l3">
-      <div class="card small">
-        <div class="card-title">#${numberResult[i]} ${nameResult[i]}</div>
-        <div class="card-image" style="overflow: initial">
-          <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${numberResult[i]}.png">
-          <a class="btn-floating halfway-fab waves-effect waves-light #1D8696"><i class="material-icons">info</i></a>
+  createCard = document.createElement("div");
+  mainBox.appendChild(createCard);
+
+  createTotal.innerHTML = `Total de tu selección: ${totalPokemon}`
+
+  newData.forEach(element => {
+    createCard.innerHTML +=
+      `<div class="col l3">
+        <div class="card small">
+        <div class="card-title">#${element.num} ${element.name}</div>
+          <div class="card-image" style="overflow: initial">
+            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${element.num}.png">
+            <a class="btn-floating halfway-fab waves-effect waves-light #1D8696"><i class="material-icons">info</i></a>
+          </div>
+          <div class="card-content" style="font-size: 16px;padding: 20px 12px;">
+            <p>${element.type.join(" - ")}</p>
+          </div>
         </div>
-        <div class="card-content" style="font-size: 16px;padding: 20px 12px;">
-          <p>${finalType.join(" - ")}</p>
-        </div>
-      </div>
-    </div>`;
-  }
+      </div>`;
+  });
 };
 
 document.getElementById("type").addEventListener("change", () => {
 
   document.getElementById("root").innerHTML = "";
 
-  const newData = sortedLower(data);
-  const numberResult = printNumber(newData);
-  const nameResult = printName(newData);
-  const imageResult = printImage(newData);
-  const typeResult = printType(newData);
-
+  let selectedOrder = document.getElementById("order").value;
   let selectedType = document.getElementById("type").value;
+  const dataOnSort = sortData(data, selectedOrder);
+  const newData = filterData(dataOnSort,selectedType);
+  const totalPokemon = computeStats(newData);
 
-  for(i=0; i<typeResult.length; i++){
-    let finalType = [];
-    if(typeResult[i][0] === selectedType || typeResult[i][1] === selectedType){
-      createCard = document.createElement("div");
-      //createCard.className = "col l2";
-      mainBox.appendChild(createCard);
-      for(a=0; a < typeResult[i].length; a++){
-        if(typeResult[i][a] === "Grass"){
-          finalType.push("Planta");
-        }else if(typeResult[i][a] === "Poison"){
-          finalType.push("Veneno");
-        }else if(typeResult[i][a] === "Fire"){
-          finalType.push("Fuego");
-        }else if(typeResult[i][a] === "Bug"){
-          finalType.push("Bicho");
-        }else if(typeResult[i][a] === "Flying"){
-          finalType.push("Volador");
-        }else if(typeResult[i][a] === "Normal"){
-          finalType.push("Normal");
-        }else if(typeResult[i][a] === "Electric"){
-          finalType.push("Eléctrico");
-        }else if(typeResult[i][a] === "Ground"){
-          finalType.push("Tierra");
-        }else if(typeResult[i][a] === "Water"){
-          finalType.push("Agua");
-        }else if(typeResult[i][a] === "Fighting"){
-          finalType.push("Lucha");
-        }else if(typeResult[i][a] === "Psychic"){
-          finalType.push("Psíquico");
-        }else if(typeResult[i][a] === "Rock"){
-          finalType.push("Roca");
-        }else if(typeResult[i][a] === "Ghost"){
-          finalType.push("Fantasma");
-        }else if(typeResult[i][a] === "Ice"){
-          finalType.push("Hielo");
-        }else if(typeResult[i][a] === "Dragon"){
-          finalType.push("Dragón");
-        }
-      }
-      createCard.innerHTML += "<h4>" + numberResult[i] + " " + nameResult[i] + "</h4>" + "<img src=" + 
-      imageResult[i] + ">" + "<p>" + finalType.join(" ") + "</p>";
-     }
-  }
+  createTotal = document.createElement("h5");
+  mainBox.appendChild(createTotal);
+
+  createCard = document.createElement("div");
+  mainBox.appendChild(createCard);
+
+  createTotal.innerHTML = `Total de tu selección: ${totalPokemon}`
+
+  newData.forEach(element => {
+    createCard.innerHTML +=
+      `<div class="col l3">
+        <div class="card small">
+          <div class="card-title">#${element.num} ${element.name}</div>
+          <div class="card-image" style="overflow: initial">
+            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${element.num}.png">
+            <a class="btn-floating halfway-fab waves-effect waves-light #1D8696"><i class="material-icons">info</i></a>
+          </div>
+          <div class="card-content" style="font-size: 16px;padding: 20px 12px;">
+            <p>${element.type.join(" - ")}</p>
+          </div>
+        </div>
+      </div>`;
+  });
 });
 
 document.getElementById("order").addEventListener("change", () => {
 
   document.getElementById("root").innerHTML = "";
   let selectedOrder = document.getElementById("order").value;
-  let newData;
-  if(selectedOrder === "az"){
-    newData = sortedAz(data);
-  }else if(selectedOrder === "za"){
-    newData = sortedZa(data);
-  }else if(selectedOrder === "lower"){
-    newData = sortedLower(data);
-  }else if(selectedOrder === "higher"){
-    newData = sortedHigher(data);
-  }
+  let selectedType = document.getElementById("type").value;
+  const dataOnFilter = filterData(data,selectedType);
+  const newData = sortData(dataOnFilter, selectedOrder);
+  const totalPokemon = computeStats(newData);
 
-  const idResult = printId(newData);
-  const numberResult = printNumber(newData);
-  const nameResult = printName(newData);
-  const imageResult = printImage(newData);
-  const typeResult = printType(newData);
+  createTotal = document.createElement("h5");
+  mainBox.appendChild(createTotal);
 
-  for(i=0; i < idResult.length; i++){
-    let finalType = [];
-    createCard = document.createElement("div");
-    createCard.className = "card";
-    mainBox.appendChild(createCard);
-    for(a=0; a < typeResult[i].length; a++){
-      if(typeResult[i][a] === "Grass"){
-        finalType.push("Planta");
-      }else if(typeResult[i][a] === "Poison"){
-        finalType.push("Veneno");
-      }else if(typeResult[i][a] === "Fire"){
-        finalType.push("Fuego");
-      }else if(typeResult[i][a] === "Bug"){
-        finalType.push("Bicho");
-      }else if(typeResult[i][a] === "Flying"){
-        finalType.push("Volador");
-      }else if(typeResult[i][a] === "Normal"){
-        finalType.push("Normal");
-      }else if(typeResult[i][a] === "Electric"){
-        finalType.push("Eléctrico");
-      }else if(typeResult[i][a] === "Ground"){
-        finalType.push("Tierra");
-      }else if(typeResult[i][a] === "Water"){
-        finalType.push("Agua");
-      }else if(typeResult[i][a] === "Fighting"){
-        finalType.push("Lucha");
-      }else if(typeResult[i][a] === "Psychic"){
-        finalType.push("Psíquico");
-      }else if(typeResult[i][a] === "Rock"){
-        finalType.push("Roca");
-      }else if(typeResult[i][a] === "Ghost"){
-        finalType.push("Fantasma");
-      }else if(typeResult[i][a] === "Ice"){
-        finalType.push("Hielo");
-      }else if(typeResult[i][a] === "Dragon"){
-        finalType.push("Dragón");
-      }
-    }
-    createCard.innerHTML += "<h4>" + numberResult[i] + " " + nameResult[i] + "</h4>" + "<img src=" + 
-    imageResult[i] + ">" + "<p>" + finalType.join(" ") + "</p>";
-  }
-});
+  createCard = document.createElement("div");
+  mainBox.appendChild(createCard);
+
+  createTotal.innerHTML = `Total de tu selección: ${totalPokemon}`
+
+  createTotal
+  newData.forEach(element => {
+    createCard.innerHTML +=
+      `<div class="col l3">
+        <div class="card small">
+          <div class="card-title">#${element.num} ${element.name}</div>
+          <div class="card-image" style="overflow: initial">
+            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${element.num}.png">
+            <a class="btn-floating halfway-fab waves-effect waves-light #1D8696"><i class="material-icons">info</i></a>
+          </div>
+          <div class="card-content" style="font-size: 16px;padding: 20px 12px;">
+            <p>${element.type.join(" - ")}</p>
+          </div>
+        </div>
+      </div>`;
+    });
+  });
