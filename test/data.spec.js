8e8505fa83d64ef5d05288de40499f2bbb605dca
amdemic <amdm1982@gmail.com>
2019-01-09 21:49:23 -0300

    se realiza test de funcion filterData existosamente

--- test/data.spec.js
+++ test/data.spec.js
@@ -3,22 +3,25 @@ global.assert = require ("chai").assert;
 require('../src/data.js');
 require("../src/data/pokemon/pokemon.js");
 
+const type = window.POKEMON.pokemon.type;
+const data = window.POKEMON.pokemon;
 
 
-
-describe ("window.POKEMON.pokemon", () => {
+describe ("data", () => {
   it ("deberia ser un objeto", () => {
-    assert.equal(typeof window.POKEMON.pokemon,"object");
+    global.assert.equal(typeof window.POKEMON.pokemon,"object");
   });
 });
 
-describe ("POKEMON.pokemon.filterData", () => {
+describe ("filterData", () => {
   it("deberia ser una funcion",() => {
-    assert.equal(typeof window.pokemonData.filterData, "function");
+    global.assert.equal(typeof window.pokemonData.filterData, "function");
   });
-  it(`deberia retornar "Bulbasaur" para "Grass" con "type[0]"`, () => {
-    assert.equal(typeof window.pokemonData.filterData ("type[0]",type.Grass), "Bulbasaur");
+  it(`deberia retornar como primer pokemon de clase Planta a Bulbasaur`, () => {
+    global.assert.equal(window.pokemonData.filterData (data,"Grass")[0].name, "Bulbasaur");
   });
 });
+
+
 
 
