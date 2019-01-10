68c57df57c3c94504c6295fa9bdb70a20ef9bb65
amdemic <amdm1982@gmail.com>
2019-01-09 22:38:42 -0300

    testeando funcion sortdata

--- test/data.spec.js
+++ test/data.spec.js
@@ -5,6 +5,8 @@ require("../src/data/pokemon/pokemon.js");
 
 const type = window.POKEMON.pokemon.type;
 const data = window.POKEMON.pokemon;
+const num = window.POKEMON.pokemon.num;
+const name = window.POKEMON.pokemon.name;
 
 
 describe ("data", () => {
@@ -22,6 +24,30 @@ describe ("filterData", () => {
   });
 });
 
+describe ("sortData", () => {
+  it("deberia ser una funcion", () => {
+    global.assert.equal(typeof window.pokemonData.sortData, "function");
+  });
+
+  it ("deberia retornar el numero 001 como el primer numero ordenado de 1-151", () => {
+    global.assert.equal(window.pokemonData.sortData (data,"num"), "1-151"[0].num,"001");
+  });
+
+  it ("deberia retornar el numero 151 como el primer numero ordenado de 151-1", () => {
+    global.assert.equal(window.pokemonData.sortData (data,"num").higher, "151-1"[151].num,"151");
+  });
+
+  it ("deberia retornar el nombre Abra como el primer nombre ordenado de a-z", () => { 
+    global.assert.equal(window.pokemonData.sortData (data,"name"), "A-Z"[0].name, "Abra");
+  });
+
+  it ("deberia retornar el nombre Zubat como el primer nombre ordenado de z-a", () => {
+    global.assert.equal(window.pokemonData.sortData (data,"name"), "Z-A"[151].name, "Zubat");
+  })
+
+
+
+});
 
 
 
